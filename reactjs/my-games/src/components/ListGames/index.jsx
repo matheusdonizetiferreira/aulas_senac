f88548e6acab4e987a470fc22b3382a38eae821f
matheusdonizetiferreira <mathferreira726@gmail.com>
2023-08-23 16:04:44 -0300

    Atualização de páginas

--- reactjs/my-games/src/components/ListGames/index.jsx
+++ reactjs/my-games/src/components/ListGames/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import './button.css';
 
-  const ItemConteiner = styled.div`
+const ItemConteiner = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -9,7 +10,6 @@ import styled from 'styled-components';
     height: 605px;
     gap: 10px;
     border-radius: 5px;
-    /* position: relative; */
     color: #29303b;
     margin-bottom: 10px;
     padding: 10px;
@@ -17,7 +17,7 @@ import styled from 'styled-components';
 
 
 
-  const Thumbnaill = styled.img`
+const Thumbnaill = styled.img`
    width: 100%;
    height: 80%;
     border-radius: 5px;
@@ -25,17 +25,23 @@ import styled from 'styled-components';
 
 `;
 
-  const Title = styled.div`
+const Title = styled.div`
     color: #ffffff;
+    text-shadow: 5px 5px 15px black;
+
+
     `;
 
-  const Price = styled.div`
+const Price = styled.div`
     color: white;
     text-align: center;
+    background-color: purple;
+    padding: 5px;
+    border-radius: 5px;
 
  `;
 
-  const Button = styled.div`
+const Button = styled.div`
   border-radius: 8px;
   border: 1px solid transparent;
   padding: 0.6em 1.2em;
@@ -53,15 +59,15 @@ import styled from 'styled-components';
   }
 `;
 
-  const ItemLink = styled.a`
+const ItemLink = styled.a`
     text-decoration: none;
 
     `;
 
 
 
-export default function ListGames({game}) {
- 
+export default function ListGames({ game }) {
+
 
   return (
     <>
@@ -69,9 +75,22 @@ export default function ListGames({game}) {
         <ItemConteiner>
           <Thumbnaill src={game.image} />
 
+
           <Title>{game.title}</Title>
-          <Price>{game.price}</Price>
-          <Button className="codepen-button">Comprar</Button>
+          {/* <Price>{game.price}</Price> */}
+          {/* <Button className="codepen-button">Comprar</Button> */}
+          <div data-tooltip= {`${game.price}`} className="button">
+            <div className="button-wrapper">
+              <div className="text">Comprar</div>
+              <span className="icon">
+                <svg viewBox="0 0 16 16" className="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+                </svg>
+              </span>
+            </div>
+          </div>
+
+
 
         </ItemConteiner>
       </ItemLink>
--- reactjs/my-games/src/pages/Home/index.jsx
+++ reactjs/my-games/src/pages/Home/index.jsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
-import styled from 'styled-components'
 import './home.css'
 import ListGames from "../../components/ListGames";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+      const navigate = useNavigate()
       const [games, setGames] = useState([]);
 
-      const getData=async() =>{
+      const getData = async () => {
             let resp = await fetch("http://localhost:3000/data")
             let Data = await resp.json()
-            
+
             setGames(Data)
-           
-      } 
+
+      }
 
 
       useEffect(() => {
@@ -20,12 +21,25 @@ function Home() {
       }, [])
       return (
             <div className="background">
-                  <h1>Compre Aqui os Melhores Jogos do Momento</h1>
-                  <div className= "containerGames">
-                        {games.map(game =>{
+                  <div className="containerBotao">
+                        <h1>Compre Aqui os Melhores Jogos do Momento</h1>
+                        <button class="btn" type="button" onClick={() => navigate('/cadastrar')}>
+                              <strong>Cadastrar Jogo</strong>
+                              <div id="container-stars">
+                                    <div id="stars"></div>
+                              </div>
+
+                              <div id="glow">
+                                    <div class="circle"></div>
+                                    <div class="circle"></div>
+                              </div>
+                        </button>
+                  </div>
+                  <div className="containerGames">
+                        {games.map(game => {
                               console.log(game.title)
                               return (
-                                    <ListGames key={game.id} game={game}/>
+                                    <ListGames key={game.id} game={game} />
                               )
                         })}
                   </div>
